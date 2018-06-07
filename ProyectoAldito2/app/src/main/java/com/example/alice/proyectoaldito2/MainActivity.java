ecbdf6b12437e390972bea9ac5886aa6b0fdfc3e
AlicePeach <alicepeachmelocoton2@gmail.com>
2018-06-06 23:50:56 -0500

    subiendo proyecto

---
+++ ProyectoAldito2/app/src/main/java/com/example/alice/proyectoaldito2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.alice.proyectoaldito2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = (ImageView) findViewById(R.id.startMarciano);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent irmenu = new Intent(MainActivity.this, MenuCarrito.class);
+                startActivity(irmenu);
+            }
+        });
+    }
+}
